8e5b15a23bc045f38c5743dade160dbca432199d
Ahmad Sherief <ahmad.sherief1@gmail.com>
2016-10-25 02:58:24 +0200

    Lesson 2

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -178,7 +178,7 @@ public class ForecastFragment extends Fragment {
                             }
 
                         for (String s : resultStrs) {
-                            Log.v(LOG_TAG, "Forecast entry: " + s);
+
                         }
                        return resultStrs;
 
@@ -220,7 +220,7 @@ public class ForecastFragment extends Fragment {
                         .build();
                 URL url = new URL(builtUri.toString());
 
-                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
+
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -248,9 +248,9 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
-                Log.v(LOG_TAG,"Forecast JSON String: ");
+
             } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
+
                 // If the code didn't successfully get the weather data, there's no point in attemping
                 // to parse it.
                 return null;
@@ -262,7 +262,7 @@ public class ForecastFragment extends Fragment {
                     try {
                         reader.close();
                     } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
+
                     }
                 }
             }
@@ -271,7 +271,7 @@ public class ForecastFragment extends Fragment {
                 return getWeatherDataFromJson(forecastJsonStr, numDays);
             }
             catch (JSONException e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
+
                 e.printStackTrace();
             }
             return null;
